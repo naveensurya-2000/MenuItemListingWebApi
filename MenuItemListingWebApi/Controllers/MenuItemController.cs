24e354397c1f9a91bfd50dcba03e51acbab75d05
naveensurya-2000 <naveens19032000@gmail.com>
2022-04-02 15:46:31 +0530

    first commit

---
+++ MenuItemListingWebApi/Controllers/MenuItemController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MenuItemListingWebApi.Models;
+
+namespace MenuItemListingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        List<MenuItem> menuItems = new List<MenuItem>()
+        {
+                new MenuItem(1,"Pizza",true,400,"2022-03-31",false),
+                new MenuItem(2,"Burger",true,200,"2022-03-26",true),
+                new MenuItem(3,"Sandwich",false,600,"2022-03-20",true),
+                new MenuItem(4,"French fries",true,400,"2022-03-31",false)
+        };
+
+        [HttpGet]
+        public List<MenuItem> Get()
+        {
+            return menuItems;
+        }
+
+        [HttpGet("{id}")]
+        public MenuItem Get(int id)
+        {
+            return menuItems[id - 1];
+        }
+    }
+}
---
+++ MenuItemListingWebApi/Models/MenuItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MenuItemListingWebApi.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Free Delivery")]
+        public bool FreeDelivery { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfLaunch { get; set; }
+        public bool Active { get; set; }
+
+        public MenuItem(int id, string name, bool freedelivery, double price, string dateoflaunch, bool active)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.FreeDelivery = freedelivery;
+            this.Price = price;
+            this.DateOfLaunch = DateTime.Parse(dateoflaunch);
+            this.Active = active;
+        }
+
+    }
+}
